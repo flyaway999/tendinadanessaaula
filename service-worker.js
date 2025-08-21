90f35a3f8b62d41500eb6c74e1e9573710451846
flyaway999 <lvdsala01l@gmail.com>
2025-08-21 11:09:56 -0300

    teste

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'ecotech-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Instalando o Service Worker e cacheando os arquivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Arquivos em cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptando as requisições de rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Retorna o recurso do cache
+        }
+        return fetch(event.request); // Faz a requisição se não estiver no cache
+      })
+  );
+});
+// Atualizando o cache quando há mudanças
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
